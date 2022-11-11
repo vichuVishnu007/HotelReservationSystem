ef09c380e9c7811339b463ba1f1862dc99a9ff2a
2106791 <2106791@cognizant.com>
2022-11-11 14:51:24 +0530

    initial commit

--- src/main/java/com/app/hotelmanagment/CustomActuatorEndpoint/CustomGitInformation.java
+++ src/main/java/com/app/hotelmanagment/CustomActuatorEndpoint/CustomGitInformation.java
@@ -1,27 +1,69 @@
 package com.app.hotelmanagment.CustomActuatorEndpoint;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class CustomGitInformation implements InfoContributor {
 
-    PropertySourcesPlaceholderConfigurer propsConfig;
+    @Value("${git.branch}")
+    private String branch;
+    @Value("${git.build.host}")
+    private String buildhost;
+    @Value("${git.build.time}")
+    private String buildTime;
+    @Value("${git.build.user.email}")
+    private String buildUseremail;
 
-    public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-        propsConfig = new PropertySourcesPlaceholderConfigurer();
-        propsConfig.setLocation(new ClassPathResource("git.properties"));
-        propsConfig.setIgnoreResourceNotFound(true);
-        propsConfig.setIgnoreUnresolvablePlaceholders(true);
-        return propsConfig;
-    }
+    @Value("${git.build.user.name}")
+    private String buildUserName;
 
+    @Value("${git.build.version}")
+    private String buildVersion;
+
+    @Value("${git.commit.committer.time}")
+    private String commitTime;
+
+    @Value("${git.commit.id}")
+    private String commitID;
+
+    @Value("${git.commit.user.email}")
+    private String commitUserEmail;
+
+    @Value("${git.commit.user.name}")
+    private String commitUserName;
+
+    @Value("${git.remote.origin.url}")
+    private String commitURL;
 
     @Override
     public void contribute(Info.Builder builder) {
-        builder.withDetail("Git Information",propsConfig);
+
+        Map<String, Object> gitInfo = new HashMap<>();
+        gitInfo.put("Branch", branch);
+        gitInfo.put("Host", buildhost);
+        gitInfo.put("BuildTime", buildTime);
+        gitInfo.put("BuildUsermail", buildUseremail);
+        gitInfo.put("BuildUserName", buildUserName);
+        gitInfo.put("BuildVersion", buildVersion);
+        gitInfo.put("CommitID", commitID);
+        gitInfo.put("CommitemailID", commitUserEmail);
+        gitInfo.put("CommitUserName", commitUserName);
+        gitInfo.put("CommitTime", commitTime);
+        gitInfo.put("CommitURL", commitURL);
+        builder.withDetail("Git commit Info", gitInfo);
     }
 }
--- src/main/java/com/app/hotelmanagment/configuration/BeanConfiguration.java
+++ src/main/java/com/app/hotelmanagment/configuration/BeanConfiguration.java
@@ -1,13 +1,16 @@
 package com.app.hotelmanagment.configuration;
 
+import com.app.hotelmanagment.CustomActuatorEndpoint.CustomGitInformation;
 import com.app.hotelmanagment.entity.APIResponse;
 import com.app.hotelmanagment.entity.Client;
 import com.app.hotelmanagment.entity.ReservationEntity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource({"git.properties"})
 public class BeanConfiguration {
 
     @Bean
