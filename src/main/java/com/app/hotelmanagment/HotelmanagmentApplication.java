0b29ee0379a14abec8a3e4c30bbab38fd0c2abdb
2106791 <2106791@cognizant.com>
2022-12-01 14:14:10 +0530

    jar commit

--- src/main/java/com/app/hotelmanagment/HotelmanagmentApplication.java
+++ src/main/java/com/app/hotelmanagment/HotelmanagmentApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
